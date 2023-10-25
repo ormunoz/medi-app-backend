14f30a408e1c6286c11c1dc0819d941280a77597
ormunoz <or.munozf@gmail.com>
2023-10-24 22:03:06 -0300

    consultas http creada

---
+++ src/helpers/http.js
@@ -0,0 +1,56 @@
+
+const sendOk = (res, message, result, code = 200) => {
+    return res.status(code).json({
+        status: true,
+        message,
+        data: result,
+    });
+}
+
+const sendOk204 = (res, message, result, code = 204) => {
+    return res.status(code).json({
+        status: true,
+        message,
+    });
+}
+
+const badRequest = (res, message, result, code = 400) => {
+    return res.status(code).json({
+        status: false,
+        message,
+        data: result
+    });
+}
+
+
+const denegateeAccess = (res, message, result, code = 401) => {
+    return res.status(code).json({
+        status: false,
+        message,
+        data: result
+    });
+}
+
+const permissonDegate = (res, message, code = 403) => {
+    return res.status(code).json({
+        status: false,
+        message
+    });
+}
+
+const internalError = (res, message, result, code = 500) => {
+    return res.status(code).json({
+        status: false,
+        message,
+        data: result
+    });
+}
+
+module.exports = {
+    sendOk,
+    badRequest,
+    internalError,
+    denegateeAccess,
+    sendOk204,
+    permissonDegate
+}
