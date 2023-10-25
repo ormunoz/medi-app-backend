f9e6ceed75915483daed9f6ca402dcbfa2c69a03
ormunoz <or.munozf@gmail.com>
2023-10-24 22:24:20 -0300

    se cambia por error de creacion

--- src/router/users.routes.js
+++ src/router/users.routes.js
@@ -1,62 +1,11 @@
-const { PrismaClient } = require('@prisma/client');
-const bcrypt = require("bcrypt");
-const jwt = require('jsonwebtoken');
-const { sendOk, badRequest, internalError, sendOk204, permissonDegate } = require("../helpers/http");
-const prisma = new PrismaClient()
-require('dotenv').config();
+const express = require('express');
+const router = express.Router();
+// const { validarToken } = require('../auth/validaToken');
+// const { validarUser } = require('../middleware/validUser');
+const { controllerUser } = require("../controller/indexController");
 
-// obtener datos del usuario logeado
-const getUserToken = async (req, res) => {
-    try {
-        return sendOk(res, "Usuario validado correctamente")
-    } catch (error) {
-        return internalError(res, "Error inesperado", error);
-    }
-};
+// User
+router.get('/all',  controllerUser.getAll);
+router.post('/one', controllerUser.getUser);
 
-// trae a un usuario en especifico
-const getUser = async (req, res) => {
-    try {
-        const { id } = req.body;
-        const user = await prisma.users.findUnique({
-            where: {
-                id: parseInt(id),
-            },
-            include: {
-                profesional: true,
-                patients: true,
-            },
-        });
-
-        if (!user) {
-            return badRequest(res, "no se encontro nigun usuario");
-        } else {
-            return sendOk(res, "Usuario agregado correctamente", user);
-        }
-    } catch (error) {
-        return internalError(res, "Error inesperado", error);
-    }
-};
-
-
-const getAll = async (req, res) => {
-    try {
-      const allUsers = await prisma.user.findMany({
-        include: {
-            profesional: true,
-            patients: true,
-        }
-      });
-  
-      return sendOk(res, "Usuario Encontrados", allUsers);
-  
-    } catch (error) {
-      res.json({ message: error.message });
-    }
-  };
-
-module.exports = {
-    getUserToken,
-    getUser,
-    getAll
-};
+module.exports = router;
