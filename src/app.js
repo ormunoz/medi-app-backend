28feb6a0fea2026fc48e76b1b20b8dad5abf4753
ormunoz <or.munozf@gmail.com>
2023-10-28 16:37:05 -0300

    se deja en puerto 5000

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ const userRoutes = require('./router/users.routes');
 const questionRoutes = require('./router/questionsOptions.routes');
 const bodyParser = require('body-parser');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.static(path.join(__dirname, './public')));
